7e14f02f347d23fb364797a8360e4763b567befb
facostap <acostamichael80@gmail.com>
2024-06-11 00:44:22 -0500

    Inicio de primer project en spring

---
+++ nextclass-plataformas/src/main/java/com/nextclass/plataformas/NextclassPlataformasApplication.java
@@ -0,0 +1,13 @@
+package com.nextclass.plataformas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NextclassPlataformasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NextclassPlataformasApplication.class, args);
+	}
+
+}
---
+++ nextclass-plataformas/src/test/java/com/nextclass/plataformas/NextclassPlataformasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nextclass.plataformas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NextclassPlataformasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
